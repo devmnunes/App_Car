0220ce46363d97544e5d85222efbb5773ff97740
Matheus Nunes <143001745+devmnunes@users.noreply.github.com>
2025-04-23 12:14:08 -0300

    comit

--- lib/paginas/home.dart
+++ lib/paginas/home.dart
@@ -9,69 +9,73 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Carro meucarro = getmeuCarro();
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Sua Garagem",
-          style: TextStyle(
-            color: const Color.fromARGB(242, 3, 3, 3),
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        backgroundColor: const Color.fromARGB(251, 2, 74, 182),
-        centerTitle: true,
-        shadowColor: Colors.black,
-        elevation: 7,
-      ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: const Color.fromARGB(255, 223, 226, 226),
-        child: Padding(
-          padding: const EdgeInsets.all(40),
-          child: Container(
-            child: Column(
-              children: [
-                Text(
-                  "Marca: " + meucarro.marca,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ),
-                ),
-                Text(
-                  "Modelo: " + meucarro.modelo,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ),
-                ),
-                Text(
-                  "Cor: " + meucarro.cor,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ),
-                ),
-                 
-                Text(
-                  "Ano: " + meucarro.ano.toString(),
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ),
-                ),
-                    Container(
-                      child: Image.network(
-                        meucarro.urlImage!,
-                        width: 550,
-                      ),
-                    ),
-                  ],
-                ),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Sua Garagem",
+            style: TextStyle(
+              color: const Color.fromARGB(242, 3, 3, 3),
+              fontWeight: FontWeight.bold,
             ),
+          ),
+          backgroundColor: const Color.fromARGB(251, 2, 74, 182),
+          centerTitle: true,
+          shadowColor: Colors.black,
+          elevation: 7,
         ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Column(
+                children: [
+                  Text(
+                    "Marca: " + meucarro.marca,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Center(
+              child: Text(
+                "Modelo: " + meucarro.modelo,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Center(
+              child: Text(
+                "Cor: " + meucarro.cor,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Center(
+              child: Text(
+                "Ano: " + meucarro.ano.toString(),
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Center(
+              child: Image.network(
+                meucarro.urlImage!,
+                width: 550,
+              ),
+            ),
+          ],
         ),
+      ),
     );
   }
 }
